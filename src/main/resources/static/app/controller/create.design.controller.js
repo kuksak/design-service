6ca4487a016401e17dc61dd319507fb9decffbf2
Sakshi Kukreti <sakshikukreti18@gmail.com>
2017-04-30 11:40:31 +0530

    added fa-spinner

--- src/main/resources/static/app/controller/create.design.controller.js
+++ src/main/resources/static/app/controller/create.design.controller.js
@@ -2,7 +2,7 @@
  * Created by Sakshi Kukreti on 30-04-2017.
  */
 angular.module('app')
-    .controller('createDesignController', function ($scope, designService) {
+    .controller('createDesignController', function ($scope, $state, designService) {
         $scope.design = {};
         $scope.design.designNum = '';
         $scope.design.author = 'random';
@@ -10,11 +10,14 @@ angular.module('app')
         $scope.saveDesign = function () {
             var array = [];
             array.push($scope.design);
+            $scope.showLoader = true;
             designService
                 .saveChanges(array)
                 .then(function (response) {
+                    $scope.showLoader = false;
                     console.log("created new design", response);
+                    $state.go('homeState');
                 });
 
-        }
+        };
     });
--- src/main/resources/static/app/controller/design.controller.js
+++ src/main/resources/static/app/controller/design.controller.js
@@ -5,20 +5,25 @@
 angular.module('app')
     .controller('designController', function ($scope, designService) {
         $scope.saveChanges = function () {
+            $scope.showLoader = true;
             designService
                 .saveChanges($scope.designs)
                 .then(function (response) {
+                    $scope.showLoader = false;
                     console.log('updating design', response);
                     initController();
                 });
         };
 
         function initController() {
+            $scope.showLoader = true;
             designService.fetchAllDesigns()
                 .then(function (response) {
+                    $scope.showLoader = false;
                     console.log(response);
                     $scope.designs = response.data;
                 });
+
         }
 
         initController();
